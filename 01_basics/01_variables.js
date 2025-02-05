401de108173a22469014b313f2df0c511336b5bc
Skeleton <77226233+SKELETONOP@users.noreply.github.com>
2025-02-05 08:02:11 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 332701;
+let accountEmail = " nareshgouttam@gmail.com";
+var accountPassword = "12345678";
+accountCity = "jaipur";
+let accountState;
+
+// accountId = 2; not allowed beacuse initail me hmne const assign kiya tha
+accountEmail= "gouttamnaresh@gmail.com"
+accountPassword = "23456"
+accountCity="Sikar"
+console.log(accountEmail);
+console.table([accountEmail, accountId,accountPassword,accountCity, accountState])
+
+// preper not to use var baecause of block scope and function scope
