1d2b4fb64aa08d2afd5d7caca06d726b45e3176a
Skeleton <77226233+SKELETONOP@users.noreply.github.com>
2025-02-08 10:16:57 +0000

    comparisions

--- 01_basics/03_conversionAndOperaters.js
+++ 01_basics/03_conversionAndOperaters.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof(score));
-console.log(typeof score)
+// console.log(typeof(score));
+// console.log(typeof score)
 
 let valueInNum = Number(score)
-console.log(typeof valueInNum)
+// console.log(typeof valueInNum)
 
 // "33" => 33
 //"33abc" => NaN (Not a Number)
@@ -12,12 +12,49 @@ console.log(typeof valueInNum)
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 ==> True, 0==> False
 // '' ==> False , "Naresh" ==> True
 
 let someNumber = 33
 let someeNumber = String(someNumber)
-console.log(someeNumber)
-console.log(typeof someeNumber)
+// console.log(someeNumber)
+// console.log(typeof someeNumber)
+
+// *************************************--- Operations ---*********************************************
+
+let value = 3
+let NegValue = -value
+// console.log(NegValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Naresh"
+let str2 = " Gouttam"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+ 2 + 2 );
+// console.log(1+ 2 + "2" );
+
+// console.log(+true);
+// console.log(+"");
+
+// let Num1, Num2, Num3
+
+// Num1 = Num2 = Num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2 >= 1 );
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// always avoid this kind of conversion
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+ 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// Either Use this === 
+
+console.log("22" === 22);
+
