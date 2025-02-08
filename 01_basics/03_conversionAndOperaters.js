49025934d33edd094e12e537bf2f47d9378e8dc2
Skeleton <77226233+SKELETONOP@users.noreply.github.com>
2025-02-08 09:14:07 +0000

    discussion of datatypes

---
+++ 01_basics/03_conversionAndOperaters.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+console.log(typeof(score));
+console.log(typeof score)
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum)
+
+// "33" => 33
+//"33abc" => NaN (Not a Number)
+//  true => 1 , False => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 ==> True, 0==> False
+// '' ==> False , "Naresh" ==> True
+
+let someNumber = 33
+let someeNumber = String(someNumber)
+console.log(someeNumber)
+console.log(typeof someeNumber)
