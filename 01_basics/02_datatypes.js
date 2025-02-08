d32e66ac5ff9770b49d0c82d5cfd25c5139f417f
Skeleton <77226233+SKELETONOP@users.noreply.github.com>
2025-02-08 08:56:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all the code with newer js
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3 + 3); console.log("naresh"); // we should also not do this
+
+// code redablilty should be high
+
+let name = " naresh";
+let age = 18;
+let isLoggedin = true;
+let state = null;
+
+// premative data types
+
+// number 2 power of 53
+// bigint is also a data type
+//  string to store an name aur some word
+//  boolean ==>  (True, False)
+// null ==> its and repentation of null value ( Khali hai)
+// undefined ==> WHICH IS NOT DEFINED
+// symbol ==> when we talk about unique we wil use symbol
+
+
+// not premative data types
+
+// object
+console.log(age)
+console.log(typeof "Naresh "); // string
+console.log(typeof null); // Object (but shows Undefined)
+
+
