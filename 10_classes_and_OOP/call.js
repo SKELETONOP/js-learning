deb73a24fe63ed77eee7669804abf4089c6a762a
Naresh Gouttam <77226233+SKELETONOP@users.noreply.github.com>
2025-04-09 12:37:44 +0000

    call in js

---
+++ 10_classes_and_OOP/call.js
@@ -0,0 +1,15 @@
+function SetUserName (username) {
+    //complex database call
+    this.username = username;
+    console.log("called");
+}
+
+function createUser (username,email,password) {
+    SetUserName.call(this, username)
+
+    this.email = email;
+    this.password = password;
+}
+
+const chai = new createUser("chai", "chai@fb.com", "1234");
+console.log(chai);
